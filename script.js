910edc7c9381573962ced8481af60325bbeb899c
John Gibson <jhngbsn09@gmail.com>
2022-07-31 15:10:16 -0400

    Created an event listener to change color

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// const gridBox = document.createElement('div');
-// gridBox.setAttribute('id','gridBox');
-// const grid = document.getElementById('grid');
-// grid.appendChild(gridBox);
 
 const grid = document.getElementById("grid");
 
-function makeGrid(rows, cols){
+function makeGrid(rows, cols){       
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++){
         let cell = document.createElement("div");
+        cell.addEventListener('mouseover', function changeBlack() {cell.style.backgroundColor = 'black';});
         grid.appendChild(cell).className = "grid-item";
-    }
+    }  
 }
 
-console.log(makeGrid(16,16));
+console.log(makeGrid(16,16))
