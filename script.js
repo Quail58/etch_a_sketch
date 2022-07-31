062eac6c05866864e71cfe4e80c102d20c605a92
John Gibson <jhngbsn09@gmail.com>
2022-07-31 13:53:54 -0400

    Created a function that makes a grid

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const gridBox = document.createElement('div');
-gridBox.setAttribute('id','gridBox');
-const grid = document.getElementById('grid');
-grid.appendChild(gridBox);
+// const gridBox = document.createElement('div');
+// gridBox.setAttribute('id','gridBox');
+// const grid = document.getElementById('grid');
+// grid.appendChild(gridBox);
+
+const grid = document.getElementById("grid");
+
+function makeGrid(rows, cols){
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++){
+        let cell = document.createElement("div");
+        grid.appendChild(cell).className = "grid-item";
+    }
+}
+
+console.log(makeGrid(16,16));
