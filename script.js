9ec3ec108176cd612e3ea5bbee725955adc24aa1
John Gibson <jhngbsn09@gmail.com>
2022-08-02 18:56:29 -0400

    Added a clear function to clear the grid

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 
 const grid = document.getElementById("grid");
 
+const reset = document.getElementsByClassName("reset");
+
+let pencilColor = "black";
+
 // makes the grid for etch a sketch
 function makeGrid(rows, cols){       
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++){
         let cell = document.createElement("div");
-        cell.addEventListener('mouseover', function changeBlack() {cell.style.backgroundColor = 'black';});
-        grid.appendChild(cell).className = "grid-item";
+        cell.style.backgroundColor = "white";
+        // event listener to see if the mouse is over and change the color of the box        
+        cell.addEventListener('mouseover', function changeBlack() {cell.style.backgroundColor = pencilColor;});
+        grid.appendChild(cell).className = "gridItem";
     }  
 }
+
+
+
+clears the grid
+function clearGrid() {
+  while(grid.firstChild){
+    grid.removeChild(grid.lastChild);
+  }
+  makeGrid(slider.value,slider.value);
+}
+
 
 // displays the value of the slider on screen
 var slider = document.getElementById("myRange");
@@ -22,4 +39,4 @@ slider.oninput = function() {
 }
 
 //runs the grid when the website loads, at the value of what the slider is set to by default
-console.log(makeGrid(slider.value,slider.value))
+console.log(makeGrid(slider.value,slider.value));
