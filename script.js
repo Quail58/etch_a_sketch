427872ca49b5ef2fa0375eee010b08a288ffe757
John Gibson <jhngbsn09@gmail.com>
2022-08-01 20:34:46 -0400

    Added notes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const grid = document.getElementById("grid");
 
+// makes the grid for etch a sketch
 function makeGrid(rows, cols){       
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
@@ -11,13 +12,14 @@ function makeGrid(rows, cols){
     }  
 }
 
+// displays the value of the slider on screen
 var slider = document.getElementById("myRange");
 var output = document.getElementById("range");
 output.innerHTML = slider.value;
-
 
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
+//runs the grid when the website loads, at the value of what the slider is set to by default
 console.log(makeGrid(slider.value,slider.value))
